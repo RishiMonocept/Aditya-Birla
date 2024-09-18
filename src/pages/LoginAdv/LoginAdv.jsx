5f633f6fb769a6ec1d24fd697e4982a647693d84
KamleshMonocept <kamleshbhatt@monocept.com>
2024-09-18 17:08:52 +0530

    feat: Login toggle buttons, OTP Input, fix: bottom Image bugs

--- src/pages/LoginAdv/LoginAdv.jsx
+++ src/pages/LoginAdv/LoginAdv.jsx
@@ -2,19 +2,24 @@ import { View, ImageBackground } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BottomImage from "./BottomImage";
+import ToggleButtons from "./ToggleButtons";
+import OTPVerification from "./OTP/OtpVerification";
 
 const LoginAdv = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 0.65, borderWidth: 2 }}>
+      <View style={{ flex: 0.65 }}>
         <View
           style={{
             marginTop: 56,
             marginHorizontal: 16,
-            borderWidth: 1,
+            // borderWidth: 1,
             flex: 1,
           }}
-        ></View>
+        >
+          <ToggleButtons />
+          <OTPVerification />
+        </View>
       </View>
       <BottomImage />
     </SafeAreaView>
---
+++ src/pages/LoginAdv/OTP/otpVerification.style.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    // justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F1F3F6",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  otpContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    // gap: 8,
+  },
+  otpInput: {
+    width: 40,
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#F1F3F6",
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    marginHorizontal: 5,
+    fontSize: 18,
+    textAlign: "center",
+  },
+  submitButton: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#C7222A",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+});
